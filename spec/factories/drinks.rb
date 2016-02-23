694100e60a677039d3070d0cc22480f04b8e7084
GQiu <imqiuqiu@gmail.com>
2016-02-23 16:23:47 +0800

    add coffess scope to Drink

--- spec/factories/drinks.rb
+++ spec/factories/drinks.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :drink do
     name { ['AAA', 'BBB', 'CCC'].sample }
-    drink_type { ['Coffee', 'Tea'].sample }
+    drink_type { ['coffee', 'tea'].sample }
   end
 end
--- spec/models/drink_spec.rb
+++ spec/models/drink_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe Drink do
   describe '#coffees' do
-    it 'returns an array of all coffee type drinks'
+    it 'returns an array of all coffee type drinks' do
+      create_list :drink, 2, drink_type: 'coffee'
+
+      expect(Drink.coffees.count).to eq 2
+    end
 
   end
 
