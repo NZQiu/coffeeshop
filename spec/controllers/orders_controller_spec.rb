9e91c22dd90ea14098dfafe40fdd6cb8b613b2a7
GQiu <imqiuqiu@gmail.com>
2016-02-23 18:11:54 +0800

    add orders controller specs

---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe OrdersController do
+  describe 'GET #index' do
+
+  end
+
+  # describe 'GET #new' do
+  # end
+
+  describe 'POST #create' do
+  end
+
+  describe 'GET #type' do
+  end
+
+  describe 'GET #size' do
+    
+  end
+end
