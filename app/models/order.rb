de017a84278dda9a6337ecfadbe04d41ba832798
GQiu <imqiuqiu@gmail.com>
2016-02-23 16:43:47 +0800

    order must belong to item

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+  belongs_to :item
+
+  validates :item, presence: true
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 describe Order do
+  describe 'creation' do
+    it 'must belongs to a item' do
+      order = create :order
+      expect(order.item).not_to be_nil
 
+      order = build :order, item: nil
+      expect(order.valid?).to be_falsy
+    end
+
+    it 'denies when item_id is invalid'
+  end
 end
