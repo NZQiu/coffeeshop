ff15b04abc4cda86526779eeb5f279b2ba6a93aa
GQiu <imqiuqiu@gmail.com>
2016-02-23 16:16:13 +0800

    add specs skeleton

---
+++ spec/models/drink_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Drink do
+  describe '#coffees' do
+    it 'returns an array of all coffee type drinks'
+
+  end
+
+  describe '#teas' do
+    it 'returns an array of all tea type drinks'
+
+  end
+end
