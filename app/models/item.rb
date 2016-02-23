ebe470918fff2d1006b4e7bf06038cbf1716a6e6
GQiu <imqiuqiu@gmail.com>
2016-02-23 16:37:56 +0800

    item must belongs to a Drink

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ActiveRecord::Base
+  belongs_to :drink
+
+  validates :drink, presence: true
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :item do
     association :drink, factory: :drink
-    price { Rand(1000) }
+    price { Random.rand(1000) }
     size { ['Tall', 'Grande', 'Venti'].sample }
   end
 end
