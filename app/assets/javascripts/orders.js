19c9725d6977e7aa7779bc069c493d486d5f48ee
GQiu <imqiuqiu@gmail.com>
2016-02-26 19:02:28 +0800

    group order by cup size and drink type

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,18 +1,42 @@
 (function($, document) {
   $().ready(function() {
-    var orderData = $('#order-data');
+    var orderData = $('#order-data'),
+      typeDdl = $('#type-ddl-menu'),
+      sizeDdl = $('#size-ddl-menu');
 
-    ajaxOrders(1);
+    ajaxOrders();
 
     $(orderData).on('click', 'ul.pagination a', function(e) {
       e.preventDefault();
       ajaxOrders(parseInt(getUrlParams($(this).attr('href')).page || 1));
     });
 
+    $(typeDdl).find('a').click(function() {
+      var drink_type = $(this).text(),
+        cur = $(typeDdl).data('q');
+      if (!cur || cur != drink_type) {
+        $(typeDdl).data('q', drink_type);
+        $('#type-text').text(drink_type);
+        ajaxOrders();
+      }
+    });
+
+    $(sizeDdl).find('a').click(function() {
+      var cup_size = $(this).text(),
+        cur = $(sizeDdl).data('q');
+      if (!cur || cur != cup_size) {
+        $(sizeDdl).data('q', cup_size);
+        $('#size-text').text(cup_size);
+        ajaxOrders();
+      }
+    });
+
     function ajaxOrders(page) {
       $('span.loading-icon').show();
       $.get($(orderData).data('url'), {
-        page: page
+        page: page || 1,
+        type: $('#type-ddl-menu').data('q') || '',
+        size: $('#size-ddl-menu').data('q') || ''
       }).done(function(data) {
         $(orderData).html(data);
         $('span.loading-icon').hide();
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,19 @@
 class OrdersController < ApplicationController
 
   ORDERS_PER_PAGE = 5
-  
+
   def index
   end
 
   def listing
-    @orders = Order.all.order(created_at: :desc).page(params[:page]).per(ORDERS_PER_PAGE)
+    drink_type = params[:type]
+    cup_size = params[:size]
+
+    @orders = Order.all
+    @orders = @orders.by_drink_type(drink_type) if drink_type && !drink_type.empty? && drink_type.downcase != 'all'
+    @orders = @orders.by_cup_size(cup_size) if cup_size && !cup_size.empty? && cup_size.downcase != 'all'
+    @orders = @orders.order(created_at: :desc).page(params[:page]).per(ORDERS_PER_PAGE) if @orders
+
     respond_to do |f|
       f.html { render layout: false }
     end
